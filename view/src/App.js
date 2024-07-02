2ad6cba05f6a48547ebf1b4c43019fca9c96fdff
Waron Limsakul <poonro@gmail.com>
2024-07-02 21:34:30 +0700

    create view folder using react

---
+++ view/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/">
+  </Route>
+));
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  );
+}
+
+export default App;
